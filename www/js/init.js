2634ec459b0dd022bc18c4bbf5e5e11675426b51
JesusSePe <umbreon20019@gmail.com>
2021-03-16 19:15:22 +0100

    today's update

--- www/js/init.js
+++ www/js/init.js
@@ -26,7 +26,7 @@ function showResults(result) {
   var bands = result["artists"];
   for (let index = 0; index < bands.length; index++) {
     const element = bands[index];
-    $('.collection:first-of-type').append('<li class="collection-item"><div>'+element["name"]+'<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>');
+    $('.collection:first-of-type').append('<li artistid="'+element["id"]+'" class="collection-item">'+element["name"]+'<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></li>');
   }
 
   $('.secondary-content').click(function() {
@@ -37,8 +37,17 @@ function showResults(result) {
     var tabs = document.getElementById("tabs");
     var tabsInstance = M.Tabs.getInstance(tabs);
     tabsInstance.select("test-swipe-2");
-    // console.log(parentText);
     
+    // Print artist info on second screen
+    $.ajax({
+      method: "GET",
+      url: "https://musicbrainz.org/ws/2/artist/"+varParent.attr("artistid"), // Artist ID on custom tag
+      dataType: "json",
+    }).done(function(msg){
+      showDetails(msg);
+    }).fail(function(){
+      alert("Ajax Error");
+    });
         /*
         // Tab 2
         var tabs = document.getElementById("tabs-swipe-demo");
@@ -46,6 +55,16 @@ function showResults(result) {
         tabsInstance.select("tab2");
         */
   });
+}
+
+function showDetails(info) {
+  $('.details').empty();
+  console.log(info);
+  console.log(info["name"]);
+  $('<h3>'+info["name"]+'</h3>').appendTo('.details');
+  $('<p><b>Type:</b> '+info["type"]+'</p>').appendTo('.details');
+  $('<p><b>Country:</b> '+info["area"]["sort-name"]+'</p>').appendTo('.details');
+  $('<p><b>Life-span:</b> '+info["life-span"]["begin"]+' to '+info["life-span"]["end"]+'</p>').appendTo('.details');
 }
 
 document.addEventListener('deviceready', onDeviceReady, false);
