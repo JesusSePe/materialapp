a9559d2ca0d7cae658c9c3719a5bf73b132db238
JesusSePe <umbreon20019@gmail.com>
2021-04-13 15:29:18 +0200

    Moar updates I forgot to upload

---
+++ www/js/db.js
@@ -0,0 +1,281 @@
+var data_info = {
+    "code": "ICC0",
+    "name": "Desenvolupament d'aplicaciones Web",
+    "hours": "2000",
+    "modules": {
+        "MP01": {
+            "code": "MP01",
+            "name": "Sistemes informàtics",
+            "hours": "165",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Instal·lació, configuració i explotació del sistema informàtic",
+                    "hours": "60"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Gestió de la informació i de recursos en una xarxa",
+                    "hours": "80"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Implantació de programari específic",
+                    "hours": "25"
+                }
+            }
+        },
+        "MP02": {
+            "code": "MP02",
+            "name": "Bases de dades",
+            "hours": "198",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Introducció a les bases de dades",
+                    "hours": "33"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Llenguatges SQL: DML i DDL",
+                    "hours": "66"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Llenguatge SQL: DCL i extensió procedimental",
+                    "hours": "66"
+                },
+                "UF4": {
+                    "code": "UF4",
+                    "name": "Bases de dades objecte relacionals",
+                    "hours": "33"
+                }
+            }
+        },
+        "MP03": {
+            "code": "MP03",
+            "name": "Programació",
+            "hours": "297",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Programació estructurada",
+                    "hours": "85"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Disseny modular",
+                    "hours": "50"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Fonaments de gestió de fitxers",
+                    "hours": "30"
+                },
+                "UF4": {
+                    "code": "UF4",
+                    "name": "Programació orientada a objectes",
+                    "hours": "55"
+                },
+                "UF5": {
+                    "code": "UF5",
+                    "name": "Llibreries de classes fonamentals",
+                    "hours": "48"
+                },
+                "UF6": {
+                    "code": "UF6",
+                    "name": "Introducció a la persistència en BD",
+                    "hours": "29"
+                }
+            }
+        },
+        "MP04": {
+            "code": "MP04",
+            "name": "Llenguatges de marques i sistemes de gestió de la informació",
+            "hours": "99",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Programació amb XML",
+                    "hours": "45"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Àmbits d'aplicació de l'XML",
+                    "hours": "27"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Sistemes de gestió d'informació empresarial",
+                    "hours": "27"
+                }
+            }
+        },
+        "MP05": {
+            "code": "MP05",
+            "name": "Entorns de desenvolupament",
+            "hours": "66",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Desenvolupament de programari",
+                    "hours": "20"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Optimització de programari",
+                    "hours": "20"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Introducció al disseny orientat a objectes",
+                    "hours": "26"
+                }
+            }
+        },
+        "MP06": {
+            "code": "MP06",
+            "name": "Desenvolupament web entorn client",
+            "hours": "140",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Sintaxis del lenguaje",
+                    "hours": "30"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Estructures definides pel programador",
+                    "hours": "30"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Model d'objectes del document",
+                    "hours": "42"
+                },
+                "UF4": {
+                    "code": "UF4",
+                    "name": "Comunicació asíncrona client-servidor",
+                    "hours": "38"
+                }
+            }
+        },
+        "MP07": {
+            "code": "MP07",
+            "name": "Desenvolupament web en entorn servidor",
+            "hours": "140",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Programació web en entorn servidor",
+                    "hours": "30"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Generació dinàmica de pàgines web",
+                    "hours": "30"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Tècniques d'accés a dades",
+                    "hours": "38"
+                },
+                "UF4": {
+                    "code": "UF4",
+                    "name": "Serveis web",
+                    "hours": "42"
+                }
+            }
+        },
+        "MP08": {
+            "code": "MP08",
+            "name": "Desplegament d'aplicacions web",
+            "hours": "56",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Servidors web i de transferència de fitxers",
+                    "hours": "26"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Servidors d'aplicacions web",
+                    "hours": "10"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Aplicacions web",
+                    "hours": "10"
+                },
+                "UF4": {
+                    "code": "UF4",
+                    "name": "Control de versions i documentació",
+                    "hours": "10"
+                }
+            }
+        },
+        "MP09": {
+            "code": "MP09",
+            "name": "Disseny d'interfícies web",
+            "hours": "84",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Disseny de la interfície",
+                    "hours": "39"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Elements multimèdia: creació i integració",
+                    "hours": "22"
+                },
+                "UF3": {
+                    "code": "UF3",
+                    "name": "Accessibilitat i usabilitat",
+                    "hours": "23"
+                }
+            }
+        },
+        "MP10": {
+            "code": "MP10",
+            "name": "Formació i orientació laboral",
+            "hours": "99",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Incorporació al treball",
+                    "hours": "66"
+                },
+                "UF2": {
+                    "code": "UF2",
+                    "name": "Prevenció de riscos laborals",
+                    "hours": "33"
+                }
+            }
+        },
+        "MP11": {
+            "code": "MP11",
+            "name": "Empresa i iniciativa emprenedora",
+            "hours": "66",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Empresa i iniciativa emprenedora",
+                    "hours": "66"
+                }
+            }
+        },
+        "MP12": {
+            "code": "MP12",
+            "name": "Projecte de desenvolupament d'aplicacions web",
+            "hours": "99",
+            "ufs": {
+                "UF1": {
+                    "code": "UF1",
+                    "name": "Projecte de desenvolupament d'aplicacions web",
+                    "hours": "99"
+                }
+            }
+        }
+    }
+};
--- www/js/init.js
+++ www/js/init.js
@@ -5,66 +5,15 @@
     $('.sidenav').sidenav();
     $('.tabs').tabs({ "swipeable": true });
   }); // end of document ready
-})(jQuery); // end of jQuery name space
+})(jQuery); // end of jQuery name space.
 
-
-$("#searchbutton").click(function() {
-  band = $('#searchbar').val();
-  $.ajax({
-    method: "GET",
-    url: "https://musicbrainz.org/ws/2/artist?query="+band,
-    dataType: "json",
-  }).done(function(msg){
-    showResults(msg);
-  }).fail(function(){
-    alert("Ajax Error");
-  });
-});
-
-function showResults(result) {
-  $('.collection:first-of-type').empty();
-  var bands = result["artists"];
-  for (let index = 0; index < bands.length; index++) {
-    const element = bands[index];
-    $('.collection:first-of-type').append('<li artistid="'+element["id"]+'" class="collection-item">'+element["name"]+'<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></li>');
-  }
-
-  $('.secondary-content').click(function() {
-    // Save the parent node in a variable
-    varParent = $(this).parent();
-    // Clone parent, delete children and get the inner text
-    parentText = varParent.clone().children().remove().end().text();
-    var tabs = document.getElementById("tabs");
-    var tabsInstance = M.Tabs.getInstance(tabs);
-    tabsInstance.select("test-swipe-2");
-    
-    // Print artist info on second screen
-    $.ajax({
-      method: "GET",
-      url: "https://musicbrainz.org/ws/2/artist/"+varParent.attr("artistid"), // Artist ID on custom tag
-      dataType: "json",
-    }).done(function(msg){
-      showDetails(msg);
-    }).fail(function(){
-      alert("Ajax Error");
+function data() {
+    jQuery.each( data_info['modules'], function( i, mp){
+        $('.collapsible').first().append('<li><div class="collapsible-header">'+mp['code']+'. '+mp['name']+'</div><div class="collapsible-body"><div class="row"></div></div></li>');
+        jQuery.each(mp['ufs'], function(x, uf){
+            $('.row').last().append('<div class="col s6 m4">'+uf['code']+'. '+uf['name']+'</div>');
+        })
     });
-        /*
-        // Tab 2
-        var tabs = document.getElementById("tabs-swipe-demo");
-        var tabsInstance = M.Tabs.getInstance(tabs);
-        tabsInstance.select("tab2");
-        */
-  });
-}
-
-function showDetails(info) {
-  $('.details').empty();
-  console.log(info);
-  console.log(info["name"]);
-  $('<h3>'+info["name"]+'</h3>').appendTo('.details');
-  $('<p><b>Type:</b> '+info["type"]+'</p>').appendTo('.details');
-  $('<p><b>Country:</b> '+info["area"]["sort-name"]+'</p>').appendTo('.details');
-  $('<p><b>Life-span:</b> '+info["life-span"]["begin"]+' to '+info["life-span"]["end"]+'</p>').appendTo('.details');
 }
 
 document.addEventListener('deviceready', onDeviceReady, false);
@@ -73,5 +22,10 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
+        data();
+    });
+    
+    
 }
